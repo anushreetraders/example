200c54ae78fbaa3f7f346e68a1846eac0bc1fce0
anushreetraders <anushree.traders7@gmail.com>
2025-09-02 11:00:17 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold">Anushree Traders & Order Suppliers</h1>
+      <p className="mt-4 text-lg">Welcome to our official website</p>
+    </div>
+  );
+}
+
+export default App;
